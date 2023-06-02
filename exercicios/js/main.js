ada452082a51973100c5ff50156450bf39a9418e
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-06-02 16:26:25 -0300

    Adicionando exemplos js e comentando code do projeto.

---
+++ exercicios/js/main.js
@@ -0,0 +1,6 @@
+
+document.body.style.background = 'black' // Altera a cor de fundo da página web para a cor preta
+
+document.body.style.color = 'white' // Altera a cor de fonte da página web
+
+alert("OláMundo") // Exibe mensagem popup na tela do usuário
