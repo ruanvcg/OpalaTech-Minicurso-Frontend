2a655673222d24b73fb7f2d7a209133d2900b174
Ruan <ruanvictorp22111@gmail.com>
2023-06-03 08:37:10 -0300

    Adicionando exemplos JS e Slide atualizado.

--- exercicios/js/main.js
+++ exercicios/js/main.js
@@ -3,4 +3,8 @@ document.body.style.background = 'black' // Altera a cor de fundo da página web
 
 document.body.style.color = 'white' // Altera a cor de fonte da página web
 
+document.querySelector('img.perfil').style.filter='grayscale(100%)'
+
+//document.querySelector('img.perfil').style.visibility = 'hidden' // Torna a foto de perfil invisível
+
 alert("OláMundo") // Exibe mensagem popup na tela do usuário
